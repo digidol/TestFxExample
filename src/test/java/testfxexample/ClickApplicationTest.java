13935aaefba5b41e50106eebbd2d9ad803bc8546
nst <nst@aber.ac.uk>
2018-04-16 16:59:30 +0100

    Added changes following advice on gitter.
    Change from TextMatchers to LabelMatchers. Also tried org.testfx.assertions.api.Assertions.assertThat as alternative ways to setup the same tests for clicking on the button.

--- src/test/java/testfxexample/ClickApplicationTest.java
+++ src/test/java/testfxexample/ClickApplicationTest.java
@@ -1,16 +1,18 @@
 package testfxexample;
 
 import static org.testfx.api.FxAssert.*;
-import static org.testfx.matcher.control.TextMatchers.*;
+import static org.testfx.matcher.control.LabeledMatchers.*;
 import static org.junit.Assert.*;
 
-import org.testfx.framework.junit.ApplicationTest;
+import static org.testfx.assertions.api.Assertions.assertThat;
 
+import org.testfx.framework.junit.ApplicationTest;
 import org.testfx.robot.Motion;
 
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import javafx.scene.control.Button;
 
 import org.junit.Test;
 
@@ -38,5 +40,28 @@ public class ClickApplicationTest extends ApplicationTest {
         
         // then:
         verifyThat(".button", hasText("clicked!"));
+    }
+    
+    /** 
+     * The following tests are alternative ways to do the matching. 
+     * They would allow an IDE to offer suggestions and let the compiler 
+     * detect problems at compile time. 
+     */
+    
+    @Test 
+    public void should_contain_button_with_assertj() {
+       assertThat(lookup(".button").queryAs(Button.class)).hasText("click me!"); 
+    }
+    
+    @Test 
+    public void should_do_click_on_button_with_assertj() {
+       clickOn(".button", Motion.DIRECT); 
+       assertThat(lookup(".button").queryAs(Button.class)).hasText("clicked!"); 
+    }
+    
+    @Test 
+    public void should_do_click_on_button_with_assertj_alternative_version() {  
+        clickOn(".button", Motion.DIRECT);
+        assertThat(lookup(".button").queryButton()).hasText("clicked!");
     }
 }
